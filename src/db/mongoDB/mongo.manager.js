b29bcec50e44a7d3098bf0799234e3d5db4862ec
Sebastian Martin <sebigran@gmail.com>
2023-06-08 18:50:31 -0300

    implementacion de logger

---
+++ src/db/mongoDB/mongo.manager.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const { DB_CONFIG } = require("../../config/dbConfig");
+const { logger } = require("../../logger/logger");
+const { PERSISTENCE } = require("../../config/env.config");
+
+//connect with the database with SINGLETON METHOD
+
+class MongoDbConnection {
+  static #instance;
+  constructor() {
+    if (PERSISTENCE === "MONGO") {
+      mongoose.set("strictQuery", false);
+
+      mongoose
+        .connect(DB_CONFIG.mongoDb.uri)
+        .then(() => {
+          logger.info("Database connection successful");
+        })
+        .catch((error) => {
+          logger.fatal("Database connection error");
+          throw error;
+        });
+    }
+  }
+  static getInstance() {
+    if (!this.#instance) {
+      this.#instance = new MongoDbConnection();
+    }
+    return this.#instance;
+  }
+}
+
+module.exports = {
+  MongoDbConnection,
+};
